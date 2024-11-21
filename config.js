7da88bd8a5439620059299f0ee8d8ce385477325
Shade949 <emasterson206@icloud.com>
2024-11-21 17:47:52 -0600

    Added passwords :)

--- config.js
+++ config.js
@@ -1,12 +1,16 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
     "gointerstellar.app": "",
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    Ethan: "Shadow206",
+    Kinsler: "MH-182",
+    Henry: "ethansfat",
+    Ben: "Benjifish2011",
+    Sadaashiv: "ADJ56SDZ23XCV9",
   },
 };
 
